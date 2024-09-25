32e228cd138ade92caac04130f8b2fbb848ca539
Jack So <sojack@gmail.com>
2024-09-24 22:38:04 -0400

    necessary steps

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+'use client'
+import { useState } from "react";
 import styles from "./page.module.css";
 
-let initialBoard = [
+let defaultBoard = [
   "72-------",
   "-5---9---",
   "----38---",
@@ -9,14 +11,26 @@ let initialBoard = [
   "--1--3---",
   "---25----",
   "---6---3-",
-  "-------19",
+  "-------19"
+]
+
+let solution = [
+  "729146358",
+  "358729146",
+  "416538792",
+  "897412563",
+  "243865971",
+  "561973824",
+  "934251687",
+  "182697435",
+  "675384219"
 ]
 
 function Cell ({value, inputID}){
   return(
     value=="-" ? 
-    <input id={inputID} type="text" className={`${styles.cell} ${styles.input}`} /> 
-    : <input id={inputID} readOnly type="text" className={styles.cell} value={value}/>
+    <input id={inputID} type="number" className={`${styles.cell} ${styles.input}`} /> 
+    : <input id={inputID} readOnly type="number" className={styles.cell} value={value}/>
   )
 }
 
@@ -39,9 +53,28 @@ function Board ({initialValues}) {
 }
 
 export default function Home() {
+
+  const [initialBoard, setInitialBoard] = useState(defaultBoard)
+
+  function clickHandler() {
+    setInitialBoard([
+      "---------",
+      "---------",
+      "---------",
+      "---------",
+      "---------",
+      "---------",
+      "---------",
+      "---------",
+      "---------",
+    ])
+    console.log(initialBoard)
+  }
+
   return (
       <main className={styles.main}>
         <Board initialValues={initialBoard}/>
+        {/* <button className={styles.editButton} onClick={clickHandler}>edit</button> */}
       </main>
   );
 }
