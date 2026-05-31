145988f3cae274c9919aa743017828411818e3ed
Jack So <sojack@gmail.com>
2026-05-31 18:29:34 -0400

    Source the starting puzzle from the bundled set

--- app/page.js
+++ app/page.js
@@ -1,23 +1,11 @@
 import Game from './Game'
+import { PUZZLES } from './lib/puzzles'
 import styles from './page.module.css'
-
-// Default puzzle, as a flat row-major givens grid (0 = empty).
-const DEFAULT_GIVENS = [
-  7, 2, 0, 0, 0, 0, 0, 0, 0,
-  0, 5, 0, 0, 0, 9, 0, 0, 0,
-  0, 0, 0, 0, 3, 8, 0, 0, 0,
-  0, 0, 0, 4, 0, 0, 5, 0, 0,
-  0, 0, 3, 0, 0, 0, 9, 0, 0,
-  0, 0, 1, 0, 0, 3, 0, 0, 0,
-  0, 0, 0, 2, 5, 0, 0, 0, 0,
-  0, 0, 0, 6, 0, 0, 0, 3, 0,
-  0, 0, 0, 0, 0, 0, 0, 1, 9,
-]
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Game givens={DEFAULT_GIVENS} />
+      <Game puzzle={PUZZLES[0]} />
     </main>
   )
 }
