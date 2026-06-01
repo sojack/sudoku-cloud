4bf20838c05d9b8646b7a64c5dfc669ad497f992
Jack So <sojack@gmail.com>
2026-05-31 21:35:34 -0400

    Render Game without a bundled puzzle

--- app/page.js
+++ app/page.js
@@ -1,11 +1,10 @@
 import Game from './Game'
-import { PUZZLES } from './lib/puzzles'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Game puzzle={PUZZLES[0]} />
+      <Game />
     </main>
   )
 }
